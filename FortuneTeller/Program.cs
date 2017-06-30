7f0974c442a5d7d9b07bd827562ec82d4dbe8511
clarecorr <clare.e.corrigan@gmail.com>
2017-06-30 12:02:25 -0400

    part one

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -10,6 +10,37 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            //Part 1
+            //Collect user input & initialize variables
+
+            Console.WriteLine("Welcome to the Virtual Fortune Teller\n");
+            Console.WriteLine("Please answer the following questions to receive your fortune\n");
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your favorite ROYGBIV color?\n(Enter \"Help\" if you need to see a list of ROYGBIV colors)");
+            string responseToColor = Console.ReadLine();
+            string color;
+            if(responseToColor.ToLower() == "help")
+            {
+                Console.WriteLine("\nThe ROYGBIV colors are as follows:\n\n   R: Red\n   O: Orange\n   Y: Yellow\n   G: Green\n   B: Blue\n   I: Indigo\n   V: Violet");
+                Console.WriteLine("\nWhat is your favorite ROYGBIV color?");
+                color = Console.ReadLine();
+            }
+            else
+            {
+                color = responseToColor;
+            }
+            Console.WriteLine("How many siblings do you have?");
+            int siblings = int.Parse(Console.ReadLine());
+            
+
+
+          
+
         }
     }
 }
